88093a6e7bd8730c5ed8855a1b520c7c6bec65ae
DozingWolf <43899290+DozingWolf@users.noreply.github.com>
2018-10-06 17:29:08 +0800

    Update python_test_closure_t1.py

--- python_test_closure_t1.py
+++ python_test_closure_t1.py
@@ -1,13 +1,22 @@
 from functools import reduce
+# def createCount():
+#     a = [0]
+#     print('type of a is',type(a))
+#     print('type of a[0] is',type(a[0]))
+#     def count():
+#         a[0] = a[0] + 1
+#         return a[0]
+#     return count
+
 def createCount():
-    def caculate(arg):
-        pass
+    a = 0
     def count():
-        return 1
+        a = a + 1
+        return a
     return count
 
 count1 = createCount()
 count2 = createCount()
 count3 = createCount()
 
-print(count1(),count2(),count3())
+print(count1(),count1(),count1(),count2(),count3())
