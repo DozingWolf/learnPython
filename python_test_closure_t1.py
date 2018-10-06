940462427e76926f6b113c14868a7fcbcb731a52
DozingWolf <43899290+DozingWolf@users.noreply.github.com>
2018-10-06 17:48:12 +0800

    Update python_test_closure_t1.py

--- python_test_closure_t1.py
+++ python_test_closure_t1.py
@@ -1,19 +1,20 @@
 from functools import reduce
-# def createCount():
-#     a = [0]
-#     print('type of a is',type(a))
-#     print('type of a[0] is',type(a[0]))
-#     def count():
-#         a[0] = a[0] + 1
-#         return a[0]
-#     return count
-
+# 利用闭包返回一个计数器函数，每次调用它返回递增整数：
 def createCount():
-    a = 0
+    a = [0]
+    print('type of a is',type(a))
+    print('type of a[0] is',type(a[0]))
     def count():
-        a = a + 1
-        return a
+        a[0] = a[0] + 1
+        return a[0]
     return count
+
+# def createCount():
+#     a = 0
+#     def count():
+#         a = a + 1
+#         return a
+#     return count
 
 count1 = createCount()
 count2 = createCount()
